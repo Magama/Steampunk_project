e35a6fd92817b81e6175d01269553964be3a8350
G.A <armassalo.gazela@gmail.com>
2015-02-11 07:57:29 +0200

    Inventory.js edited
    Trying to write something...

--- Assets/Scripts/Menu-Inventory system Scripts/Inventory.js
+++ Assets/Scripts/Menu-Inventory system Scripts/Inventory.js
@@ -2,7 +2,26 @@
 
 var healthPotion : int = 0;
 var tpPotion : int = 0;
+var weapon : int = 0;
+var armor : int = 0;
 
-function Update () {
+private var showGUI : boolean = false;
 
+function Update() {
+	if(Input.GetButtonDown("Fire2")) {
+	//	showGUI = !showGUI;
+		Debug.Log("Button B!");
+	}
+	
+	//jos inventori auki, nämä eivät liiku
+	if(showGUI == true) {
+		Time.timeScale = 0;
+//		GameObject.Find("").GetComponent().enabled = false;
+	}
+	
+	//jos inventori kiinni, nämä liikkuvat taas
+	if(showGUI == false) {
+		Time.timeScale = 1;
+//		GameObject.Find("").GetComponent().enabled = true;
+	}
 }
