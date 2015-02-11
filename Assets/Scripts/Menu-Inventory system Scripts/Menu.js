e8a405ad5b466ef92fa71fb6755eb1ef0653f758
G.A. <armassalo.gazela@gmail.com>
2015-02-11 14:51:17 +0200

    Updating City scene Menu system + separated BattleArena to two different scenes

---
+++ Assets/Scripts/Menu-Inventory system Scripts/Menu.js
@@ -0,0 +1,23 @@
+﻿private var showMenu : boolean = false;
+var menu : GameObject;
+
+function Update() {
+	if(Input.GetButtonDown("Fire2")) {
+	showMenu = !showMenu;
+		Debug.Log("Button B!");
+	}
+	
+	//jos menu auki, nämä eivät liiku
+	if(showMenu == true) {
+		Time.timeScale = 0;
+		menu.gameObject.SetActive(true);
+//		GameObject.Find("CanvasMenu").GetComponent().enabled = false;
+	}
+	
+	//jos menu kiinni, nämä liikkuvat taas
+	if(showMenu == false) {
+		Time.timeScale = 1;
+		menu.gameObject.SetActive(false);
+//		GameObject.Find("CanvasMenu").enabled = true;
+	}
+}
