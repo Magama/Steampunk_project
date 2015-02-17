12bd04e22952c1b47b00dd87f1a7fd5219791ef5
G.A <armassalo.gazela@gmail.com>
2015-02-17 08:09:20 +0200

    Starting on QuestSystem and little editing

---
+++ Assets/Scripts/QuestSystem/NPCManager.js
@@ -0,0 +1,17 @@
+﻿public class NPCManager {
+//Name of ID
+	var npcID : int;
+//The Stage of the npc (Depending on the Stage the Quest is at)
+	var npcStage : int;
+//Name of NPC
+	var npcName : String;
+//The dialogue of the npc (Depending on the Stage the NPC is at)
+	var npcDialogue : String;
+	
+	function NPCManager(ID : int, Stage : int, Name : String, Dialogue : String){
+		this.npcID = ID;
+		this.npcStage = Stage;
+		this.npcName = Name;
+		this.npcDialogue = Dialogue;
+	}
+}
---
+++ Assets/Scripts/QuestSystem/QuestManager.js
@@ -0,0 +1,27 @@
+﻿public class QuestManager {
+	var questID : int;
+//Tracks what stage the quest is at
+	var questStage : int;
+//Quest name
+	var questName : String;
+//Quest details
+	var questDetails : String;
+//If the quest is started or not
+	var questStarted : boolean;
+//If the quest can be turned in or not
+	var questTurnin : boolean;
+//If the quest is finished or not AKA Has already been turned in
+	var questFinished : boolean;
+//If the quest is clicked on or not
+	var questSelected : boolean;
+
+	function QuestManager(ID : int, Stage : int, Name : String, Details : String, Started : boolean, TurnIn : boolean, Finished : boolean, Selected : boolean){
+		this.questID = ID;
+		this.questStage = Stage;
+		this.questDetails = Details;
+		this.questStarted = Started;
+		this.questTurnin = TurnIn;
+		this.questFinished = Finished;
+		this.questSelected = Selected;
+	}
+}
