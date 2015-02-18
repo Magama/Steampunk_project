5a379198b61a2d2e32353cbe03cfc4c4b3108d09
G.A <armassalo.gazela@gmail.com>
2015-02-18 08:28:57 +0200

    NPC Quest system started

---
+++ Assets/Scripts/QuestSystem/NPCs/NPCs.js
@@ -0,0 +1,19 @@
+﻿import System.Collections.Generic;
+
+//The list of all NPCs
+var NPCList : List.<NPCManager> = new List.<NPCManager>();
+
+//Commenting out, will be used later!
+//var _quest : Quests;
+
+function Start() {
+	NPC();
+}
+
+function NPC() {
+	//Dorey (the NPCs name)
+	//0
+	NPCList.Add(new NPCManager(1, 0, "Dorey", ""));
+	//1 (second NPC), etc.
+	NPCList.Add(new NPCManager(1, 0, "Elinor", ""));
+}
